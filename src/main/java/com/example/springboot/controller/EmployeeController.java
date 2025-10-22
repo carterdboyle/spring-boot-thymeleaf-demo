203507321a5e053ec0324c1a80a6d3964b832eb4
carterdboyle <94463154+carterdboyle@users.noreply.github.com>
2025-10-22 15:28:56 -0300

    Add employee form and edit employee form wired up

--- src/main/java/com/example/springboot/controller/EmployeeController.java
+++ src/main/java/com/example/springboot/controller/EmployeeController.java
@@ -1,10 +1,14 @@
 package com.example.springboot.controller;
 
+import com.example.springboot.model.Employee;
 import com.example.springboot.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class EmployeeController {
@@ -17,6 +21,31 @@ public class EmployeeController {
     public String viewHomePage(Model model) {
         model.addAttribute("listEmployees", employeeService.getAllEmployees());
         return "index";
+    }
+
+    @GetMapping("/showNewEmployeeForm")
+    public String showNewEmployeeForm(Model model) {
+        // Create model attribute to bind form data
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+        return "new_employee";
+    }
+
+    @PostMapping("/saveEmployee")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+        // save employee to database
+        employeeService.saveEmployee(employee);
+        return "redirect:/";
+    }
+
+    @GetMapping("/showFormForUpdate/{id}")
+    public String showFormForUpdate(@PathVariable(value = "id") long id, Model model) {
+        // Get the employee from the service based on the id
+        Employee employee = employeeService.getEmployeeById(id);
+
+        // set employee as a model attribute to pre-populate the form
+        model.addAttribute("employee", employee);
+        return "update_employee";
     }
 
 }
--- src/main/java/com/example/springboot/service/EmployeeService.java
+++ src/main/java/com/example/springboot/service/EmployeeService.java
@@ -1,10 +1,13 @@
 package com.example.springboot.service;
 
 import com.example.springboot.model.Employee;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 public interface EmployeeService {
     List<Employee> getAllEmployees();
+
+    void saveEmployee(Employee employee);
+
+    Employee getEmployeeById(long id);
 }
